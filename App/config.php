7d1002427e2e8b46b364b1d073767b29f52fedcd
Vladimir Buinoff <buinoff@gmail.com>
2021-12-13 00:17:54 +0200

    Homework (08) on the topic: Custom error and exception handlers

---
+++ App/config.php
@@ -0,0 +1,14 @@
+<?php
+// General settings
+define('ROOT', __DIR__ . '/../');
+error_reporting(E_ALL | E_STRICT);
+define('HOME', $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']));
+define('SHOW_ERRORS', 1);
+define('LOG_FILE_PATH', ROOT . 'logs/errors.log');
+define('LOG_DATE', '[' . date("d-M-Y H:i:s e") . ']');
+ini_set('log_errors', 'On');
+ini_set('display_errors', 0);
+date_default_timezone_set("Europe/Kiev");
+session_set_cookie_params(1440, '/', '', true, true);
+//ini_set('error_log', LOG_FILE_PATH);
+session_start();
