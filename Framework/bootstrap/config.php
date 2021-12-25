4cc4c2dba49370fc5f5e7364aa745f8fcb805f1d
Vladimir Buinoff <buinoff@gmail.com>
2021-12-25 19:28:16 +0200

    Try to fix session ID and zlib.output_compression

--- Framework/bootstrap/config.php
+++ Framework/bootstrap/config.php
@@ -1,12 +1,13 @@
 <?php
 // General settings
 define('ROOT', __DIR__ . '/../../');
-error_reporting(E_ALL | E_STRICT);
 define('HOME', $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']));
 define('SHOW_ERRORS', 1);
 define('SHOW_EXCEPTIONS', 1);
 define('LOG_PATH', ROOT . 'logs/');
 define('LOG_DATE', '[' . date("Y-m-d H:i:s 'e'") . ']');
+error_reporting(E_ALL | E_STRICT);
+ini_set("zlib.output_compression", "Off");
 ini_set('max_execution_time', 60);
 ini_set('log_errors', 'On');
 ini_set('error_log', LOG_PATH . 'errors.log');
